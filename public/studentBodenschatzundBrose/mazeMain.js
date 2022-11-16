2f6f8976ceb1c99be000790c0bf5b9ffac6b0b66
LuisBrose <luisbrose1@gmail.com>
2022-11-16 17:41:55 +0100

    Klassen erstellt.

--- public/studentBodenschatzundBrose/mazeMain.js
+++ public/studentBodenschatzundBrose/mazeMain.js
@@ -1,4 +1,11 @@
 window.onload=()=>{
-    alert("MOOOOOoIIIIiin");
-
 }
+
+class personfull{name;things}
+class person{name}
+class thing{name}
+class position{name;color;description;directions;persons;things}
+class door{closable;open;locked}
+class doorAction{action;key}
+class error{error}
+class errorWithPosition{error;position}
