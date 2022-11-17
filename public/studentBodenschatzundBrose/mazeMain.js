e8bd1223dbdf927fa9cb277a7525202b47cbd2ee
LuisBrose <luisbrose1@gmail.com>
2022-11-17 18:51:59 +0100

    -bewegen in andere Räume umgesetzt -Türen verändern umgesetzt

--- public/studentBodenschatzundBrose/mazeMain.js
+++ public/studentBodenschatzundBrose/mazeMain.js
@@ -1,6 +1,6 @@
-window.onload=()=>{
+window.onload=async ()=>{
 }
-
+//http://localhost:3000/studentBodenschatzundBrose/theMaze.html
 class personfull{name;things}
 class person{name}
 class thing{name}
@@ -9,3 +9,32 @@ class door{closable;open;locked}
 class doorAction{action;key}
 class error{error}
 class errorWithPosition{error;position}
+
+function update(){
+
+}
+
+async function changeDoorState(direction,action,key){
+    let response = await fetch('/api/door/'+direction, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            action: action,
+            key: key
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    let result = await response.json();
+}
+
+async function goToNextRoom(direction){
+    let response = await fetch('/api/person?'+new URLSearchParams({go:direction}), {
+        method: 'PATCH',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    let result = await response.json();
+    window.alert(result.error.toString());
+}
